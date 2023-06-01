631826aaabc37b84dbc8a72a4d49290d5609974a
xiayudev <jctuesta7@gmail.com>
2023-05-31 18:47:28 -0700

    fix: using one print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,5 @@ def uppercase(str):
         if (i == last_index):
             last_chr = "\n"
         if ord(letter) >= 97 and ord(letter) < 123:
-            print("{}".format(chr(ord(letter) - 32)), end=last_chr)
-        else:
-            print("{}".format(letter), end=last_chr)
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end=last_chr)
