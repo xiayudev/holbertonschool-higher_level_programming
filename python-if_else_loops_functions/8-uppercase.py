4a44672b1e6a215f0dbb407e2f032ac5af2e3591
xiayudev <jctuesta7@gmail.com>
2023-05-31 17:55:00 -0700

    fix: use only two prints

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for letter in str:
+    last_i = len(str) - 1
+    for i, letter in enumerate(str):
         if ord(letter) >= 97 and ord(letter) < 123:
-            print("{}".format(chr(ord(letter) - 32)), end="")
+            print("{}".format(
+                chr(ord(letter) - 32) if last_i != i
+                else chr(ord(letter) - 32) + "\n"),
+                end="")
         else:
-            print("{}".format(letter), end="")
-    else:
-        print()
+            print("{}".format(
+                letter if last_i != i else letter + "\n"),
+                end="")
