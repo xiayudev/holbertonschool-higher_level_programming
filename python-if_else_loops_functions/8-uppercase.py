0c8393753c0e548dcf912602b0ccddb390938d1b
xiayudev <jctuesta7@gmail.com>
2023-05-31 18:03:59 -0700

    fix: last character must be line feed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    last_i = len(str) - 1
+    last_index = len(str) - 1
+    last_chr = ""
     for i, letter in enumerate(str):
+        if (i == last_index):
+            last_chr = "\n"
         if ord(letter) >= 97 and ord(letter) < 123:
-            print("{}".format(
-                chr(ord(letter) - 32) if last_i != i
-                else chr(ord(letter) - 32) + "\n"),
-                end="")
+            print("{}".format(chr(ord(letter) - 32)), end=last_chr)
         else:
-            print("{}".format(
-                letter if last_i != i else letter + "\n"),
-                end="")
+            print("{}".format(letter), end=last_chr)
