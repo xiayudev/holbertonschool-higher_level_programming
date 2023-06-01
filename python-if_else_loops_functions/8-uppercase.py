5b3cf52d5744ce3f7120f841779698583f684c8f
xiayudev <jctuesta7@gmail.com>
2023-05-31 18:49:32 -0700

    fix: change line feed for empty string in last print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    last_index = len(str) - 1
-    last_chr = ""
     for i, letter in enumerate(str):
-        if (i == last_index):
-            last_chr = "\n"
         if ord(letter) >= 97 and ord(letter) < 123:
             letter = chr(ord(letter) - 32)
-        print("{}".format(letter), end=last_chr)
+        print("{}".format(letter), end="")
+    print("")
