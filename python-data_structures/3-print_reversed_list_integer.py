f848f004d6edca8978cdd237fdfa7d07ed6d074d
xiayudev <jctuesta7@gmail.com>
2023-06-06 16:43:32 -0700

    fix: use while loop to print in reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[num]))
+    index = len(my_list) - 1
+    while index >= 0:
+        print("{:d}".format(my_list[index]))
+        index -= 1
