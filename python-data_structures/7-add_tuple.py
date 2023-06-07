13f95b5d9c6ece2c28cae398f349dcc2c659c49d
xiayudev <jctuesta7@gmail.com>
2023-06-06 21:46:31 -0700

    fix: verify both tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,12 +4,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     sum_right = 0
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
-            tuple_a = 0,
-        tuple_a = tuple_a[0], 0
-    elif len(tuple_b) < 2:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) < 2:
         if len(tuple_b) == 0:
-            tuple_b = 0,
-        tuple_b = tuple_b[0], 0
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
     sum_left = tuple_a[0] + tuple_b[0]
     sum_right = tuple_a[1] + tuple_b[1]
     return (sum_left, sum_right)
