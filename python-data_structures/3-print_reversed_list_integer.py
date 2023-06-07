cd34886d38a6bd80c59645fcdc7747ca0c7c8936
xiayudev <jctuesta7@gmail.com>
2023-06-06 20:12:35 -0700

    fix: add a condition if list does not exit

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = sorted(my_list)
-    index = len(my_list) - 1
-    while index >= 0:
-        print("{:d}".format(new_list[index]))
-        index -= 1
+    if my_list:
+        index = len(my_list) - 1
+        while index >= 0:
+            print("{:d}".format(my_list[index]))
+            index -= 1
