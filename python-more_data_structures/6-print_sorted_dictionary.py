11c7cd3e73c28682c40a1d0d887c921384a8bb02
xiayudev <jctuesta7@gmail.com>
2023-06-10 16:44:48 -0700

    fix: add two lines before inline comment

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new = sorted([k for k in a_dictionary.keys()]) # Get keys and sort them
+    new = sorted([k for k in a_dictionary.keys()])  # Get keys and sort them
     for key in new:
         print(f"{key}: {a_dictionary[key]}")
