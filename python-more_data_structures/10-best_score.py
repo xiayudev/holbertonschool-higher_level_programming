2e9c17c9df7bd5a493a174aea190dd18b3844041
xiayudev <jctuesta7@gmail.com>
2023-06-10 18:51:27 -0700

    fix: change base value to compare

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        big_value = a_dictionary[list(a_dictionary)[0]]  # Get the first value
+        big_value = -9999
         key = ""
         for k, val in a_dictionary.items():
             if val > big_value:
                 big_value = val
                 key = k
-        if key:
+        if big_value:
             return key
-        return None
