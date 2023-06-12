0d3fcb39b9dfefae8543569910be2ccaae15da03
xiayudev <jctuesta7@gmail.com>
2023-06-11 18:24:28 -0700

    fix: incorrect use of loop inside a try statement

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,23 +2,24 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = None
     new = list()
-    try:
-        for i in range(list_length):
-            num1 = my_list_1[i]
-            num2 = my_list_2[i]
-            if num2 == 0:
-                new.append(0)
-                print("division by 0")
-                continue
-            elif not isinstance(num1, int) or not isinstance(num2, int):
-                new.append(0)
-                print("wrong type")
-                continue
-            result = num1 / num2
+    """Catch an error
+    Catch an error, if there is one, every division made with two lists.
+    If we try to put the loop inside a try, once an error is caught, the
+     |_ loop is finished.
+    """
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
             new.append(result)
-    except IndexError:
-        new.append(0)
-        print("out of range")
-    finally:
-        pass
+        except ZeroDivisionError:
+            new.append(0)
+            print("division by 0")
+        except IndexError:
+            new.append(0)
+            print("out of range")
+        except TypeError:
+            new.append(0)
+            print("wrong type")
+        finally:
+            pass
     return new
