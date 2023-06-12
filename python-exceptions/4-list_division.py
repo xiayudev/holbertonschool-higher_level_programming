d0f6cf1b93a9902611a54e51990812cfc7ccf497
xiayudev <jctuesta7@gmail.com>
2023-06-11 17:47:43 -0700

    fix: do not use given length parameter

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = None
     new = list()
     try:
-        for i in range(len(my_list_1)):
+        for i in range(list_length):
             num1 = my_list_1[i]
             num2 = my_list_2[i]
             if my_list_2[i] == 0:
@@ -16,9 +16,8 @@ def list_division(my_list_1, my_list_2, list_length):
                 continue
             result = my_list_1[i] / my_list_2[i]
             new.append(result)
-    except Exception:
-        pass
-    finally:
+    except IndexError:
         new.append(0)
         print("out of range")
-    return new
+    finally:
+        return new
