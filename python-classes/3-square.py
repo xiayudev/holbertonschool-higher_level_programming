794fab4a67a2f97b54a2ec1b7f783d596f84cd86
xiayudev <jctuesta7@gmail.com>
2023-06-14 18:53:17 -0700

    feat: add a method that calculates the area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Create a class named Square"""
+
+
+class Square:
+    """A class with a constructor and an instance attribute size"""
+
+    def __init__(self, size=0):
+        """Constructor of the class with one argument
+
+        Check if size is integer, if so raise an exception error.
+
+        Args:
+            size (int): The size of the square
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculate the area of a square
+
+        Returns:
+            int: The area of a square
+
+        """
+        return self.__size * self.__size
