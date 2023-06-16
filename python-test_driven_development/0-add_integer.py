e4305d3bbb1ad954a9a848c9bed350f5a232c04e
xiayudev <jctuesta7@gmail.com>
2023-06-15 17:20:12 -0700

    feat: add more test cases with boolean, tuple, list

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,14 +19,11 @@ def add_integer(a, b=98):
         The sum of 'a' and 'b'
 
     """
-    if not isinstance(a, (int, float)):
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    elif type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
 
-    if type(a) == float:
-        a = int(a)
-    elif type(b) == float:
-        b = int(b)
-
+    a = int(a)
+    b = int(b)
     return a + b
