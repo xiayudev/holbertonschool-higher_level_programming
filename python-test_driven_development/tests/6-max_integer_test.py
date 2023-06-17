136c06e410641d66c06f5157dec931d07c4255d3
xiayudev <jctuesta7@gmail.com>
2023-06-17 16:22:30 -0700

    fix: document functions

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,12 +6,16 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """Testing for a function that returns a max value"""
     def test_max(self):
+        """Test for verifying the max value"""
         # test max when in the list 5 is max
         self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
         self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer(), None)
         self.assertAlmostEqual(max_integer([5, 5, 5, 5, 5]), 5)
+
     def test_values(self):
+        """Test for assertion raises"""
         self.assertRaises(TypeError, max_integer, None)
         self.assertRaises(Exception, max_integer, "Hola")
