754685ff44b74e29a23c64718e6b28300882cd0a
xiayudev <jctuesta7@gmail.com>
2023-06-17 13:31:32 -0700

    feat: print text according to delimiters

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This is a module that contains a function
+
+This function is going to be used to make the project of TDD
+"""
+
+
+def text_indentation(text):
+    """Print lines with 2 new lines
+
+    Print a text with a specific condition:
+        If a '.', '?' and ':' were found, add 2 new lines
+
+    Args:
+        text (str): The text to be used in this problem
+
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    delimeter = ".?:"
+    flag = 0
+    new = str()
+
+    for lett in text:
+        if lett in delimeter:
+            flag = 1
+            new += lett
+            new = new.strip()
+            print(f"{new}", end="\n\n")
+            new = str()
+            continue
+        new += lett
+
+    if not flag:
+        print(f"{new}")
