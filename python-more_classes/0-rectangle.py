7f00c73590d3b613c9f6f154306c262274f080a6
xiayudev <jctuesta7@gmail.com>
2023-06-18 13:57:33 -0700

    feat: add an empty class called Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Creates an empty class named Rectangle"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle with no body"""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,6 @@
+# More classes
+
+In this project we will cover:
++ classes, methods, attributes
++ functions, loops
++ decorators, instances
