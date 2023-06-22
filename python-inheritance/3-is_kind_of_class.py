666715a617c014ccf835421e30be3a8f99d1a621
xiayudev <jctuesta7@gmail.com>
2023-06-22 14:31:35 -0700

    feat: check if an object is an instaces of a class or superclass

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to check an object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if an object is an instance of a class"""
+    return isinstance(obj, a_class)
