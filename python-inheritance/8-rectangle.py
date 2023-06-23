97a7f3f71d1e73b0200d083067db1d610c710cb5
xiayudev <jctuesta7@gmail.com>
2023-06-22 17:40:20 -0700

    fix: import BaseGeometry class using __import__

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,19 +3,7 @@
 """
 
 
-class BaseGeometry:
-    """A class about a base geometry"""
-
-    def area(self):
-        """Computes the area of a geometry"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate an integer containing in value"""
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -23,7 +11,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Init method that set height and width"""
-        self.__width = width
-        self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
