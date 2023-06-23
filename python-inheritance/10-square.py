8a94871c300e4fc6469e16667a11b4343b463d66
xiayudev <jctuesta7@gmail.com>
2023-06-22 18:28:25 -0700

    fix: add documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
     """A class inherited from Rectangle"""
 
     def __init__(self, size):
+        """Init method for Square class"""
         super().__init__(size, size)
         super().integer_validator("size", size)
         self.__size = size
