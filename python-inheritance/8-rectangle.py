8ae462a4c6c099d0f26d19fad0e2091d27fd580f
xiayudev <jctuesta7@gmail.com>
2023-06-22 17:03:36 -0700

    feat: inherit a class BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module to work with a Geometry class
+"""
+
+
+class BaseGeometry:
+    """A class about a base geometry"""
+
+    def area(self):
+        """Computes the area of a geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate an integer containing in value"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A class inherited from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Init method that """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
