6dde6438f9a59f5dab4706a7255ea2b50f53d118
xiayudev <jctuesta7@gmail.com>
2023-06-24 08:51:05 -0700

    feat: read a file text

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to read a file"""
+
+
+def read_file(filename=""):
+    """Read the entire file using with keyword
+
+    Args:
+        filename (str): The name of the file to read
+
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
---
+++ python-input_output/README.md
@@ -0,0 +1,6 @@
+# Python Input/Output
+
+In this project we will cover:
++ Manipulate files (read, write, append)
++ functions, classes
++ working with command lines arguments
