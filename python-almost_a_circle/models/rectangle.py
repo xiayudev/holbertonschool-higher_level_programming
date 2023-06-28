ec17bf877d4ba4efcf2d523ecf64b1b9cda53ff5
xiayudev <jctuesta7@gmail.com>
2023-06-28 16:29:51 -0700

    feat: update display method by using x and y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print the Rectangle with # character"""
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
+            for j in range(self.__x):
+                print(" ", end="")
             print("#" * self.__width, end="")
             print()
 
