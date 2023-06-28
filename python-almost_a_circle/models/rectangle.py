a78cc797c7b8402f24a054d90b0a70999dec17a2
xiayudev <jctuesta7@gmail.com>
2023-06-27 20:31:23 -0700

    feat: make attributes private

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,10 +20,10 @@ class Rectangle(Base):
 
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
