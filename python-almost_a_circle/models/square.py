75ece7733de1105389f27708efa01088a0210b8c
xiayudev <jctuesta7@gmail.com>
2023-06-28 19:20:31 -0700

    feat: create a class Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module to work with class Square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor with 4 arguments
+
+        Args:
+            size (int): The width of the Square
+            x (int): Position x
+            y (int): Position y
+            id (int): The id of the Square
+
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Return human readable string"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
