260559d51b7072cf45cd4cd47f47c884cda6e880
xiayudev <jctuesta7@gmail.com>
2023-06-29 08:23:39 -0700

    fix: add an else to return the JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,6 +26,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return a JSON string representation"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or not list_dictionaries:
             return '"[]"'
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
