935ffb3dc2cda4359830c812e7d5e14312619419
xiayudev <jctuesta7@gmail.com>
2023-06-29 07:37:04 -0700

    fix: add a validation for empty list is static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return a JSON string representation"""
-        if not list_dictionaries:
+        if list_dictionaries is None or not list_dictionaries:
             return '"[]"'
         return json.dumps(list_dictionaries)
