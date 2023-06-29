b67118fa75b9050bc5e765f7d6c1c260e73a5d2d
xiayudev <jctuesta7@gmail.com>
2023-06-28 19:44:46 -0700

    fix: use size in __str__ method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,7 +24,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return human readable string"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     @property
     def size(self):
