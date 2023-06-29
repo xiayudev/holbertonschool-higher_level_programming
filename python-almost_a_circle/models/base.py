0fe34c4e57b964156331d58ee322a7460040e012
xiayudev <jctuesta7@gmail.com>
2023-06-29 08:12:15 -0700

    fix: change condition for empty list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return a JSON string representation"""
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return '"[]"'
         return json.dumps(list_dictionaries)
