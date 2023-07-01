ee05cc645028a562322b50cbaac2d94fde2c573d
xiayudev <jctuesta7@gmail.com>
2023-07-01 16:56:10 -0700

    feat: add case for from_json_string method

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,26 +7,34 @@ import json
 from models.base import Base
 
 
-class TestToJsonString(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """Testin for a class Base"""
 
-    def test_to_json_string_empty(self):
-        """Test when Empty list is passed"""
-        self.assertEqual("[]", Base.to_json_string([]))
-
-    def test_to_json_string_None(self):
-        """Test when None is passed"""
-        self.assertEqual("[]", Base.to_json_string(None))
+    def test_to_init(self):
+        """Test for the init method"""
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(1, b1.id)
+        self.assertEqual(12, b2.id)
+        self.assertEqual(2, b3.id)
 
     def test_to_json_string(self):
-        """Test when a list of dictionaries is passed"""
-        r1 = Base()
-        r2 = Base(12)
+        """Test for the to_json_string method"""
+        self.assertEqual("[]", Base.to_json_string([]))
+        self.assertEqual("[]", Base.to_json_string(None))
         js = json.dumps([{'widht': 4, 'height': 5}])
         self.assertEqual(js, Base.to_json_string([{'widht': 4, 'height': 5}]))
         self.assertEqual(str, type(js))
-        self.assertEqual(1, r1.id)
-        self.assertEqual(12, r2.id)
+
+    def test_from_json_string(self):
+        """Test for the from_json_string method"""
+        str_json = '[{"widht": 4, "height": 5}]'
+        self.assertEqual([], Base.from_json_string(""))
+        self.assertEqual([], Base.from_json_string(None))
+        js = json.loads(str_json)
+        self.assertEqual(js, Base.from_json_string(str_json))
+        self.assertEqual(list, type(js))
 
 
 if __name__ == '__main__':
