8c3dc12615d73cc71a16618b83ca3f724afc3bef
xiayudev <jctuesta7@gmail.com>
2023-07-01 16:20:40 -0700

    feat: add new cases for Base class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -18,6 +18,16 @@ class TestToJsonString(unittest.TestCase):
         """Test when None is passed"""
         self.assertEqual("[]", Base.to_json_string(None))
 
+    def test_to_json_string(self):
+        """Test when a list of dictionaries is passed"""
+        r1 = Base()
+        r2 = Base(12)
+        js = json.dumps([{'widht': 4, 'height': 5}])
+        self.assertEqual(js, Base.to_json_string([{'widht': 4, 'height': 5}]))
+        self.assertEqual(str, type(js))
+        self.assertEqual(1, r1.id)
+        self.assertEqual(12, r2.id)
+
 
 if __name__ == '__main__':
     unittest.main()
