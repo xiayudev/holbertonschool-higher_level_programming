55a26519adc9c586a9df7355884b63674a446ede
xiayudev <jctuesta7@gmail.com>
2023-07-01 15:35:14 -0700

    fix: first result, second function

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -13,12 +13,14 @@ class TestBase(unittest.TestCase):
 
     def test_to_json_string(self):
         b1 = Base()
+        b2 = Base(12)
         js = json.dumps([{'width': 4, 'height': 5}])
 
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b1.to_json_string([]), "[]")
-        self.assertEqual(b1.to_json_string(None), "[]")
-        self.assertEqual(b1.to_json_string([{'width': 4, 'height': 5}]), js)
+        self.assertEqual(1, b1.id)
+        self.assertEqual(12, b2.id)
+        self.assertEqual("[]", b1.to_json_string([]))
+        self.assertEqual("[]", b1.to_json_string(None))
+        self.assertEqual(js, b1.to_json_string([{'width': 4, 'height': 5}]))
 
 
 if __name__ == '__main__':
