0872b328f74b1b161eb63080540f25297405141f
xiayudev <jctuesta7@gmail.com>
2023-07-01 15:50:59 -0700

    fix: add new name for the class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,15 +7,13 @@ import json
 from modules.base import Base
 
 
-class TestBase(unittest.TestCase):
+class TestToJsonString(unittest.TestCase):
     """Testin for a class Base"""
 
     def test_to_json_string(self):
         """Test for to_json_string"""
         js = json.dumps([{'width': 4, 'height': 5}])
 
-        self.assertEqual(1, Base.id)
-        self.assertEqual(12, Base.id)
         self.assertEqual("[]", Base.to_json_string([]))
         self.assertEqual("[]", Base.to_json_string(None))
         self.assertEqual(js, Base.to_json_string([{'width': 4, 'height': 5}]))
