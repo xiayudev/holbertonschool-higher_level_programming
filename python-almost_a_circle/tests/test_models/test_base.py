afe84880a7edd362ecbbdf540273a8512bcc262f
xiayudev <jctuesta7@gmail.com>
2023-07-01 15:41:54 -0700

    fix: do not put shebang at first line

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """Unittest for Base class
 """
 
@@ -12,15 +11,14 @@ class TestBase(unittest.TestCase):
     """Testin for a class Base"""
 
     def test_to_json_string(self):
-        b1 = Base()
-        b2 = Base(12)
+        """Test for to_json_string"""
         js = json.dumps([{'width': 4, 'height': 5}])
 
-        self.assertEqual(1, b1.id)
-        self.assertEqual(12, b2.id)
-        self.assertEqual("[]", b1.to_json_string([]))
-        self.assertEqual("[]", b1.to_json_string(None))
-        self.assertEqual(js, b1.to_json_string([{'width': 4, 'height': 5}]))
+        self.assertEqual(1, Base.id)
+        self.assertEqual(12, Base.id)
+        self.assertEqual("[]", Base.to_json_string([]))
+        self.assertEqual("[]", Base.to_json_string(None))
+        self.assertEqual(js, Base.to_json_string([{'width': 4, 'height': 5}]))
 
 
 if __name__ == '__main__':
