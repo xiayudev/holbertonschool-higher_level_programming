8c8bb9cf770ab91e5c5534f9664fe57bf340c30f
xiayudev <jctuesta7@gmail.com>
2023-07-01 15:22:59 -0700

    fix: correct import and call to main

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,18 +5,20 @@
 
 import unittest
 import json
-from modules.base import Base.to_json_string
+from modules.base import Base
 
 
-class TestToJsonString(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """Testin for a class Base"""
 
     def test_to_json_string(self):
-        x = Base(4, 5)
+        b1 = Base()
         js = json.dumps([{'width': 4, 'height': 5}])
-        self.assertEqual(to_json_string([]), "[]")
-        self.assertEqual(to_json_string(None), "[]")
-        self.assertEqual(to_json_string([{'width': 4, 'height': 5}]), js)
-        self.assertRaises(Exception, to_json_string(5))
-        self.assertRaises(Exception, to_json_string("5"))
-        self.assertRaises(Exception, to_json_string(x))
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b1.to_json_string([]), "[]")
+        self.assertEqual(b1.to_json_string(None), "[]")
+        self.assertEqual(b1.to_json_string([{'width': 4, 'height': 5}]), js)
+
+if __name__=='__main__':
+	unittest.main()
