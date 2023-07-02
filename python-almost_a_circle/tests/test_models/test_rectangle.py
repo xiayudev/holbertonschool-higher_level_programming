b81501d247555785d2137295fe3595844a179f31
xiayudev <jctuesta7@gmail.com>
2023-07-02 10:20:17 -0700

    feat: add cases for to_dictionary method

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,9 +3,10 @@
 
 
 import unittest
+import io
+import pycodestyle
 from unittest.mock import patch
 from unittest import mock
-import io
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -20,7 +21,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(4, r1.height)
         self.assertEqual(0, r1.x)
         self.assertEqual(0, r1.y)
-        self.assertEqual(16, r1.id)
+        self.assertEqual(19, r1.id)
         self.assertRaises(TypeError, Rectangle, "3", 4)
         self.assertRaises(TypeError, Rectangle, 3, "4")
         self.assertRaises(ValueError, Rectangle, -1, 3)
@@ -33,7 +34,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(5, r2.height)
         self.assertEqual(2, r2.x)
         self.assertEqual(1, r2.y)
-        self.assertEqual(18, r2.id)
+        self.assertEqual(21, r2.id)
         self.assertRaises(TypeError, Rectangle, 4, 5, "2", 1)
         self.assertRaises(TypeError, Rectangle, 4, 5, 2, "1")
         self.assertRaises(ValueError, Rectangle, 4, 5, -2, 1)
@@ -55,6 +56,11 @@ class TestRectangle(unittest.TestCase):
         with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
             print(r1)
         assert fake_stdout.getvalue() == rst
+        r2 = Rectangle(10, 11, 12)
+        rst_2 = "[Rectangle] (16) 12/0 - 10/11\n"
+        with mock.patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+            print(r2)
+        assert fake_stdout.getvalue() == rst_2
 
     def test_update(self):
         """Test for the update method"""
@@ -80,13 +86,33 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.y, 20)
         self.assertEqual(r2.width, 20)
         self.assertEqual(r2.height, 20)
-        self.assertEqual(r2.id, 20)
+        self.assertEqual(r2.id, 23)
         r2.update(x=3, id=100, height=7, y=4, width=10)
         self.assertEqual(r2.x, 3)
         self.assertEqual(r2.y, 4)
         self.assertEqual(r2.width, 10)
         self.assertEqual(r2.height, 7)
         self.assertEqual(r2.id, 100)
+
+    def test_to_dictionary(self):
+        """Test for the to_dictionary method"""
+        r1 = Rectangle(2, 5, 4, 4)
+        r1_dict = r1.to_dictionary()
+        dict_ = {'width': 2, 'x': 4, 'id': 17, 'height': 5, 'y': 4}
+        self.assertEqual(dict, type(r1_dict))
+        r2 = Rectangle(10, 10)
+        r2.update(**r1_dict)
+        self.assertEqual(r1.width, r2.width)
+        self.assertEqual(r1.height, r2.height)
+        self.assertFalse(r1 == r2)
+        self.assertFalse(r1 is r2)
+
+    def test_pycodestyle_conformance(self):
+        """Test for PEP8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/rectangle.py',
+                                    'tests/test_models/test_rectangle.py'])
+        self.assertEqual(result.total_errors, 0, "Found errors")
 
 
 if __name__ == '__main__':
